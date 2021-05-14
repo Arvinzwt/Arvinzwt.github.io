f62648745effc0a5587d75bd3e98481ddb89f207
Arvinzwt <zhangwentao577614@gmail.com>
2021-05-14 14:43:18 +0800

    update .gitignore

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { css } from "@emotion/react"
 import {useStaticQuery, Link, graphql} from "gatsby";
+import {StaticImage} from "gatsby-plugin-image"
+
 
 export default function Layout({children}) {
     const data = useStaticQuery(
@@ -15,18 +16,24 @@ export default function Layout({children}) {
             `
     )
 
-    return <div>
-        <div css={css`border: 1px solid red;`}>
-            头
-            <Link to={'/'}>
-                <h3>
-                    {data.site.siteMetadata.title}
-                </h3>
-            </Link>
+    return <div className='wmm'>
+        <header className='wmm-header'>
+            <h3 className='left'>
+                <StaticImage src="../images/icon.png" alt="logo" width={30} height={30}/>
+                <span> {data.site.siteMetadata.title}</span>
+            </h3>
+            <p className='right'>
+                <input type="text"/>
+                <span>目录</span>
+                <span>关于</span>
+                <span>GitHub</span>
+            </p>
+        </header>
+        <div className='wmm-content'>
+            {children}
         </div>
-        {children}
-        <div css={css`border: 1px solid green;`}>
-            尾巴
-        </div>
+        <footer className='wmm-footer'>
+            {/*尾巴*/}
+        </footer>
     </div>
 }
