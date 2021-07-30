585556e5f97db9d0f0388ddfc6d8d6db858dbd43
Arvinzwt <zhangwentao577614@gmail.com>
2021-07-30 15:22:39 +0800

    首页、添加three

---
+++ layouts/blank.vue
@@ -0,0 +1,22 @@
+<template>
+    <!--blank-->
+    <el-main class="-blank">
+        blank
+    </el-main>
+</template>
+
+<script>
+export default {
+    components: {},
+    data() {
+        return {}
+    },
+    mounted() {
+    },
+    methods: {}
+}
+</script>
+
+<style lang="scss">
+
+</style>
